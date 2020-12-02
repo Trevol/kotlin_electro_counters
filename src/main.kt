c532d5766bdc6f551401f7d2280c4bd6656e65eb
Vladimir <iam.vladimir.tretyak@gmail.com>
2020-12-02 10:41:42 +0300

    initial commit

---
+++ src/main.kt
@@ -0,0 +1,13 @@
+import nu.pattern.OpenCV
+import org.opencv.core.CvType
+import org.opencv.core.Mat
+import org.opencv.core.Scalar
+import org.opencv.highgui.HighGui
+
+fun main(){
+    OpenCV.loadLocally()
+    val m = Mat(200, 200, CvType.CV_8UC3, Scalar(23.0, 189.0, 123.0))
+    HighGui.imshow("123", m)
+    HighGui.waitKey()
+    System.exit(0)
+}
