0ee871f7bba15a99c1cd86bf59e0cb0e929a26a8
Vladimir <iam.vladimir.tretyak@gmail.com>
2020-12-02 11:59:05 +0300

    initial commit

--- src/main.kt
+++ src/main.kt
@@ -3,11 +3,12 @@ import org.opencv.core.CvType
 import org.opencv.core.Mat
 import org.opencv.core.Scalar
 import org.opencv.highgui.HighGui
+import kotlin.system.exitProcess
 
-fun main(){
+fun main() {
     OpenCV.loadLocally()
     val m = Mat(200, 200, CvType.CV_8UC3, Scalar(23.0, 189.0, 123.0))
     HighGui.imshow("123", m)
     HighGui.waitKey()
-    System.exit(0)
+    exitProcess(0)
 }
